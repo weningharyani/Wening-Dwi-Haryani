45a1f08fdf3d9ed529fc59f681a84c749daba38c
weningharyani <43592671+weningharyani@users.noreply.github.com>
2018-09-27 14:09:35 +0700

    Add files via upload

---
+++ WENING/CalculatorApp/CalculatorApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            var b = Convert.ToInt32(textBox2.Text);
+            Double hasil;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    hasil = a + b;
+                    textBox3.Text = Convert.ToString(hasil);
+                    break;
+                case 1:
+                    hasil = a - b;
+                    textBox3.Text = Convert.ToString(hasil);
+                    break;
+                case 2:
+                    hasil = a * b;
+                    textBox3.Text = Convert.ToString(hasil);
+                    break;
+                case 3:
+                    hasil = a / b;
+                    textBox3.Text = Convert.ToString(hasil);
+                    break;
+            }
+        }
+    }
+}
+
